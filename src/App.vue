900007bc9281a49a503ebcc650a0a4bcc94bf6e3
Erika Carvalho <erika.carvalho@protonmail.com>
2021-01-31 19:32:12 -0300

    add Home component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Header/>
+    <Home/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Home from './components/Home'
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Home
   }
 }
 </script>
